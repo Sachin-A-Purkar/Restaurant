e21b16714545fcb3be6e387a731442877b7053e3
Sachin <sahinpurkar0001@gmail.com>
2025-01-01 11:48:14 +0530

    Header changes

--- restaurant2/src/App.js
+++ restaurant2/src/App.js
@@ -1,31 +1,13 @@
-import React from 'react'
-import "./Header.css"
+import logo from './logo.svg';
+import './App.css';
+import Header from './head/Header';
 
-export default function Header(prop) {
+function App() {
   return (
-    <>
-    <nav>
-      <div className='conatiner-fluid'>
-        <div className='row' id='r1'>
-          <div className='col-2'>
-          <img src={prop.img1} alt='logo' id='logo'/>
-          <h1 id="loh1">Restoran</h1>
-          </div>
-          <div className='col-3'></div>
-          <div className='col-6' id='navbar1'>
-              <a href='#' >HOME</a>
-              <a href='#'>ABOUT</a>
-              <a href='#'>SERVICES</a>
-              <a href='#'>MENU</a>
-              <a href='#'>PAGES</a>
-              <a href='#'>CONTACT</a>
-          </div>
-          <div className='col-1' id='navbar'>
-            <div className='butt'><a href='#' id='buth'>BOOK A TABLE</a></div>
-          </div>
-        </div>
-      </div>
-    </nav>
-    </>
-  )
+    <div className="App">
+     <Header/>
+    </div>
+  );
 }
+
+export default App;
--- restaurant2/src/head/Header.jsx
+++ restaurant2/src/head/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import "./Header.css"
 
-export default function Header() {
+export default function Header(prop) {
   return (
-    <div>
-      Hi i am sachin
-    </div>
+    <>
+    <nav>
+      <div className='conatiner-fluid'>
+        <div className='row' id='r1'>
+          <div className='col-2'>
+          <img src={prop.img1} alt='logo' id='logo'/>
+          <h1 id="loh1">Restoran</h1>
+          </div>
+          <div className='col-3'></div>
+          <div className='col-6' id='navbar1'>
+              <a href='#' >HOME</a>
+              <a href='#'>ABOUT</a>
+              <a href='#'>SERVICES</a>
+              <a href='#'>MENU</a>
+              <a href='#'>PAGES</a>
+              <a href='#'>CONTACT</a>
+          </div>
+          <div className='col-1' id='navbar'>
+            <div className='butt'><a href='#' id='buth'>BOOK A TABLE</a></div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    </>
   )
 }
