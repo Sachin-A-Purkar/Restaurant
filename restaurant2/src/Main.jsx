cbef1262e63f41c677c2ad02bff34b889f29ded4
Sachin <sahinpurkar0001@gmail.com>
2025-01-04 14:25:47 +0530

    Main faile

---
+++ restaurant2/src/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './Head/Header'
+
+
+export default function Main() {
+    return (
+      <div className='body'>
+        <Header img1={i1}/>
+        <Body image={i2}/>
+        <Second/>
+        <Third img3={i3} img4={i4} img5={i5} img6={i6}/>
+        <Fourth img7={i7} img8={i8} img9={i9} img10={i10} img11={i11} img12={i12} img13={i13} img14={i14} img15={i15}/>
+        <Fifth img16={i7}/>
+        <Sixth img17={i16} img18={i17} img19={i18} img20={i19} img7={i7}/>
+       <Seventh img16={i7}/>
+       <Eight img21={i20} img22={i21} img23={i22} img24={i23} />
+      </div>
+    )
+  }
+  
